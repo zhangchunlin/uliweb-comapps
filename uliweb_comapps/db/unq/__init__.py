4bd4535e8c54b0783cb29c08aa0e0cc6a181ea1d
Chunlin Zhang <zhangchunlin@gmail.com>
2018-07-13 12:00:02 +0800

    unq: add :mem: support, fix function path

--- uliweb_comapps/db/unq/__init__.py
+++ uliweb_comapps/db/unq/__init__.py
@@ -1,8 +1,15 @@
 
+class _v(object):
+    mem_unqlite = None
+
 def get_unqlite(path = None, name = "default"):
     from uliweb import settings
     from unqlite import UnQLite
-    if path:
-        return UnQLite(path)
-    else:
-        return UnQLite(settings.UNQLITE_DB_NAMES.get(name))
+    if not path:
+        path = settings.UNQLITE_DB_NAMES.get(name)
+    if path==':mem:':
+        #always use a same unqlite of ':mem:'
+        if not _v.mem_unqlite:
+            _v.mem_unqlite = UnQLite(path)
+        return _v.mem_unqlite
+    return UnQLite(path)
