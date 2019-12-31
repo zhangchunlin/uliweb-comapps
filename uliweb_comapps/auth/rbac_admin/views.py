733a76c4ea4f794e59dfb2ae1d1f481dc27e37d6
Chunlin Zhang <zhangchunlin@gmail.com>
2019-12-31 14:23:11 +0800

    add user_admin and rbac_admin

---
+++ uliweb_comapps/auth/rbac_admin/views.py
@@ -0,0 +1,214 @@
+#coding=utf-8
+from uliweb import expose, functions, models
+from uliweb.i18n import ugettext_lazy as _
+from json import loads as json_loads
+
+@expose('/admin/roles')
+class RoleView(object):
+    def __begin__(self):
+        if not (request.user and request.user.is_superuser):
+            if request.function.find("api_") != -1:
+                return json({"success": False, "msg": "unauthorized"}, status=401)
+            error(_('superuser needed'))
+
+    @expose('')
+    def list(self):
+        role = "ADMIN"
+        tconfig = functions.get_apijson_table(role,"roles")
+        return {
+            "tconfig_json":json_dumps(tconfig),
+            "role":role,
+        }
+    
+    def view(self):
+        id_ = request.values.get("id")
+        if not id_:
+            error(_('No role id'))
+        
+        role = "ADMIN"
+        tconfig = functions.get_apijson_table(role,"roles")
+
+        Role = models.role
+        User = models.user
+        role_ = Role.get(id_)
+        if not role_:
+            error(_('Role "%s" not found'%(id_)))
+
+        users = [i for i in role_.users]
+        init_users = [i.id for i in users]
+        init_user_labels = ["%s (%s)"%(i.username,i.nickname) for i in users]
+
+        groups = [i for i in role_.usergroups]
+        init_groups = [i.id for i in groups]
+        init_group_labels = [i.name for i in groups]
+
+        permissions = [i for i in role_.permissions]
+        init_permissions = [i.id for i in permissions]
+        init_permission_labels = [i.name for i in permissions]
+
+
+        return {
+            "request_tag":"role",
+            "id_":id_,
+            "tconfig_json":json_dumps(tconfig),
+            "role":role,
+
+            "init_users":json_dumps(init_users),
+            "init_user_labels":json_dumps(init_user_labels),
+
+            "init_groups":json_dumps(init_groups),
+            "init_group_labels":json_dumps(init_group_labels),
+
+            "init_permissions":json_dumps(init_permissions),
+            "init_permission_labels":json_dumps(init_permission_labels),
+        }
+    
+    def api_update_users(self):
+        request_data = json_loads(request.data)
+        role_id = request_data.get("role")
+        if not role_id:
+            return json({"success": False, "msg": "no role id"})
+        if not isinstance(role_id,int):
+            return json({"success": False, "msg": "role id should be int"})
+        users = request_data.get("users")
+        if not isinstance(users,list):
+            return json({"success": False, "msg": "no user list for role"})
+        Role = models.role
+        User = models.user
+        r = Role.get(role_id)
+        if not r:
+            return json({"success": False, "msg": "cannot found role '%s'"%(role_id)})
+        user_objs = []
+        for i in users:
+            o = User.get(i)
+            if not o:
+                return json({"success": False, "msg": "cannot found user '%s'"%(i)})
+            user_objs.append(o)
+        ret = r.users.update(user_objs)
+        if ret:
+            return json({"success": True, "msg": "update role '%s' successfully"%(r.name)})
+        else:
+            return json({"success": False, "msg": "fail to update role '%s', maybe no change"%(r.name)})
+
+    def api_update_groups(self):
+        request_data = json_loads(request.data)
+        role_id = request_data.get("role")
+        if not role_id:
+            return json({"success": False, "msg": "no role id"})
+        if not isinstance(role_id,int):
+            return json({"success": False, "msg": "role id should be int"})
+        groups = request_data.get("groups")
+        if not isinstance(groups,list):
+            return json({"success": False, "msg": "no group list for role"})
+        Role = models.role
+        UserGroup = models.usergroup
+        r = Role.get(role_id)
+        if not r:
+            return json({"success": False, "msg": "cannot found role '%s'"%(role_id)})
+        group_objs = []
+        for i in groups:
+            o = UserGroup.get(i)
+            if not o:
+                return json({"success": False, "msg": "cannot found group '%s'"%(i)})
+            group_objs.append(o)
+        ret = r.usergroups.update(group_objs)
+        if ret:
+            return json({"success": True, "msg": "update role '%s' successfully"%(r.name)})
+        else:
+            return json({"success": False, "msg": "fail to update role '%s', maybe no change"%(r.name)})
+
+    def api_update_permissions(self):
+        request_data = json_loads(request.data)
+        role_id = request_data.get("role")
+        if not role_id:
+            return json({"success": False, "msg": "no role id"})
+        if not isinstance(role_id,int):
+            return json({"success": False, "msg": "role id should be int"})
+        permissions = request_data.get("permissions")
+        if not isinstance(permissions,list):
+            return json({"success": False, "msg": "no permission list for role"})
+        Role = models.role
+        Permission = models.permission
+        r = Role.get(role_id)
+        if not r:
+            return json({"success": False, "msg": "cannot found role '%s'"%(role_id)})
+        permission_objs = []
+        for i in permissions:
+            o = Permission.get(i)
+            if not o:
+                return json({"success": False, "msg": "cannot found permission '%s'"%(i)})
+            permission_objs.append(o)
+        ret = r.permissions.update(permission_objs)
+        if ret:
+            return json({"success": True, "msg": "update role '%s' successfully"%(r.name)})
+        else:
+            return json({"success": False, "msg": "fail to update role '%s', maybe no change"%(r.name)})
+
+@expose('/admin/permissions')
+class PermissionView(object):
+    def __begin__(self):
+        if not (request.user and request.user.is_superuser):
+            error(_('superuser needed'))
+
+    @expose('')
+    def list(self):
+        role = "ADMIN"
+        tconfig = functions.get_apijson_table(role,"permissions")
+        return {
+            "tconfig_json":json_dumps(tconfig),
+            "role":role,
+        }
+    
+    def view(self):
+        id_ = request.values.get("id")
+        if not id_:
+            error(_('No permission id'))
+        role = "ADMIN"
+        tconfig = functions.get_apijson_table(role,"permissions")
+
+        Permission = models.permission
+        Role = models.role
+        perm = Permission.get(id_)
+        if not perm:
+            error(_('Permission "%s" not found'%(id_)))
+
+        roles = [i for i in perm.perm_roles.all()]
+        init_roles = [i.id for i in roles]
+        init_role_labels = [i.name for i in roles]
+
+        return {
+            "request_tag":"permission",
+            "id_":id_,
+            "tconfig_json":json_dumps(tconfig),
+            "role":role,
+
+            "init_roles":json_dumps(init_roles),
+            "init_role_labels":json_dumps(init_role_labels),
+        }
+
+    def api_update_roles(self):
+        request_data = json_loads(request.data)
+        perm_id = request_data.get("permission")
+        if not perm_id:
+            return json({"success": False, "msg": "no role id"})
+        if not isinstance(perm_id,int):
+            return json({"success": False, "msg": "role id should be int"})
+        roles = request_data.get("roles")
+        if not isinstance(roles,list):
+            return json({"success": False, "msg": "no role list for role"})
+        Role = models.role
+        Permission = models.permission
+        perm = Permission.get(perm_id)
+        if not perm:
+            return json({"success": False, "msg": "cannot found role '%s'"%(perm_id)})
+        role_objs = []
+        for i in roles:
+            o = Role.get(i)
+            if not o:
+                return json({"success": False, "msg": "cannot found role '%s'"%(i)})
+            role_objs.append(o)
+        ret = perm.perm_roles.update(role_objs)
+        if ret:
+            return json({"success": True, "msg": "update permission '%s' successfully"%(perm.name)})
+        else:
+            return json({"success": False, "msg": "fail to update permission '%s', maybe no change"%(perm.name)})
---
+++ uliweb_comapps/auth/user_admin/views.py
@@ -0,0 +1,198 @@
+#coding=utf-8
+from uliweb import expose, functions, models, decorators
+from uliweb.i18n import ugettext_lazy as _
+
+def _get_portrait_image_filename(id):
+    import os
+    return os.path.join('portraits', str(id) + '.tmp' + '.jpg')
+
+def _get_portrait_image_thumbnail(id, size=50):
+    import os
+    return os.path.join('portraits', str(id) + '.%dx%d' % (size, size) + '.jpg')
+
+def get_user_image(user, size=50):
+    from uliweb.contrib.staticfiles import url_for_static
+    from uliweb import functions
+    import os
+    
+    if user:
+        image = functions.get_filename(_get_portrait_image_thumbnail(user.id, size))
+        if os.path.exists(image):
+            image_url = functions.get_href(_get_portrait_image_thumbnail(user.id, size))
+            return image_url
+
+    return functions.url_for_static('images/user%dx%d.jpg' % (size, size))
+
+_timezone_options = []
+def get_timezone_options():
+    global _timezone_options
+    if _timezone_options:
+        return _timezone_options
+    else:
+        from pendulum import timezones, from_timestamp
+        def _get_label(t):
+            offset = from_timestamp(0, t).offset_hours
+            sign = "+" if (offset>=0) else "-"
+            offset = abs(offset)
+            i = int(offset)
+            f = offset -i
+            return "%s [%s%02d:%02d]"%(t,sign,i,int(f*60+0.5))
+        _timezone_options = []
+        for i in timezones:
+            try:
+                _timezone_options.append({"value":i,"label":_get_label(i)})
+            except Exception as e:
+                pass
+        return _timezone_options
+
+@expose('/user')
+class UserView(object):
+    def __begin__(self):
+        functions.require_login()
+
+    def view(self):
+        user = request.user
+        if not user:
+            error(_('User is not exists!'))
+        User = models.user
+        UserGroup = models.usergroup
+        can_modify = True
+        image_url = functions.get_user_image(user)
+
+        role = "OWNER"
+        tconfig = functions.get_apijson_table(role,"userself")
+        tconfig["editable"] = can_modify
+
+        response.template = "UserAdmin/view.html"
+        return {
+            "request_tag":"userself",
+            "can_modify":can_modify,
+            "image_url":image_url,
+            "uid":user.id,
+            "tconfig_json":json_dumps(tconfig),
+            "role":role,
+            "usergroups":list(user.groups.all().order_by('name'))
+        }
+    
+    def edit_image(self):
+        from .forms import UploadImageForm
+        import os
+        from PIL import Image
+
+        file_image = request.files.get('image')
+        if file_image:
+            filename = _get_portrait_image_filename(request.user.id)
+            functions.save_file(filename, file_image, replace=True, convert=False)
+
+        image = _get_portrait_image_filename(request.user.id)
+        f = functions.get_filename(image)
+        if os.path.exists(f):
+            url = functions.get_href(image)
+            img = Image.open(f)
+            template_data = {'image_url':url, 'size':img.size}
+        else:
+            url = None
+            template_data = {'image_url':url, 'size':(0, 0)}
+
+        return template_data
+
+    def save_image(self):
+        from uliweb.utils.image import crop_resize
+        from uliweb.contrib.upload import get_filename
+
+        x = int(request.POST.get('x'))
+        y = int(request.POST.get('y'))
+        w = int(request.POST.get('w'))
+        h = int(request.POST.get('h'))
+
+        of = get_filename(_get_portrait_image_filename(request.user.id))
+        f = get_filename(_get_portrait_image_thumbnail(request.user.id, size=50))
+        crop_resize(of, f, x, y, w, h, size=(50, 50))
+        f = get_filename(_get_portrait_image_thumbnail(request.user.id, size=20))
+        crop_resize(of, f, x, y, w, h, size=(20, 20))
+        flash(_('Save portrait successful'))
+        return redirect(url_for(UserView.view))
+
+    def change_password(self):
+        User = models.user
+        user_id = request.GET.get('user_id', None)
+        data = {}
+        if user_id:
+            user = User.get(int(user_id))
+            if user:
+                data = {'username':user.username}
+        from .forms import ChangePasswordForm1, ChangePasswordForm2
+        if request.user:
+            form = ChangePasswordForm1()
+        else:
+            form = ChangePasswordForm2(data=data)
+        if request.method == 'GET':
+            return {'form':form, 'ok':False}
+        if request.method == 'POST':
+            flag = form.validate(request.POST)
+            if flag:
+                User = get_model('user')
+                if user_id:
+                    user = User.get(User.c.username == form.username.data)
+                    user.set_password(form.password.data)
+                    user.save()
+                    flash(_('Password saved successfully.'))
+                    return redirect('/login?next=/')
+                else:
+                    request.user.set_password(form.password.data)
+                    request.user.save()
+                    flash(_('Password saved successfully.'))
+                    return {'form':form, 'ok':True}
+            else:
+                if '_' in form.errors:
+                    message = form.errors['_']
+                else:
+                    message = _('There are something wrong, please fix them.')
+                flash(message, 'error')
+                return {'form':form, 'ok':False}
+
+@expose('/admin/users')
+class UserAdmin(object):
+    def __begin__(self):
+        functions.require_login()
+        if not request.user.is_superuser:
+            error(_('error: superuser role needed!'))
+
+    def list(self):
+        role = "ADMIN"
+        tconfig = functions.get_apijson_table(role,"users")
+        return {
+            "tconfig_json":json_dumps(tconfig),
+            "role":role,
+        }
+    
+    def view(self):
+        uid = request.values.get("id")
+        if not uid:
+            error(_('No user id'))
+        User = models.user
+        UserGroup = models.usergroup
+        user = User.get(int(uid))
+        if not user:
+            error(_('User is not exists!'))
+
+        can_modify = user.id == request.user.id
+        image_url = functions.get_user_image(user)
+
+        role = "ADMIN"
+        tconfig = functions.get_apijson_table(role,"users")
+        return {
+            "request_tag":"user",
+            "can_modify":can_modify,
+            "image_url":image_url,
+            "uid":uid,
+            "tconfig_json":json_dumps(tconfig),
+            "role":role,
+            "usergroups":list(user.groups.all().order_by('name'))
+        }
+
+@expose('/resign')
+def resign():
+    from uliweb.contrib.auth import logout
+    logout()
+    return redirect(url_for('login', next=request.referrer or '/'))
